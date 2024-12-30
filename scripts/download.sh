b418479181ba55801da307f830a2f3bb0435b7df
Xiaokun Luan <luanxiaokun@foxmail.com>
2024-12-30 13:36:34 +0800

    update scripts

--- scripts/download.sh
+++ scripts/download.sh
@@ -14,15 +14,14 @@ huggingface-cli download --repo-type dataset Salesforce/wikitext --local-dir dat
 mkdir -p models
 
 models=(
-    # "meta-llama/Llama-3.2-1B"
-    # "meta-llama/Llama-3.2-3B"
-    # "meta-llama/Llama-2-13b-hf"
+    "meta-llama/Llama-3.2-1B"
+    "meta-llama/Llama-3.2-3B"
     "meta-llama/Llama-2-7b-hf"
     "meta-llama/Llama-3.1-8B"
-    "Qwen/Qwen1.5-7B"
-    "Qwen/Qwen2.5-7B"
     "google/gemma-7b"
     "mistralai/Ministral-8B-Instruct-2410"
+    "stabilityai/stablelm-2-12b"
+    "meta-llama/Llama-2-13b-hf"
 )
 for model_path in "${models[@]}"; do
     huggingface-cli download --repo-type model "$model_path" --local-dir "models/$model_path"
--- scripts/finetune_models.sh
+++ scripts/finetune_models.sh
@@ -8,7 +8,6 @@ num5m=5000000
 num10m=10000000
 num50m=50000000
 num100m=100000000
-num200m=200000000
 
 generate_log_file() {
     local model_path=$1
@@ -16,10 +15,10 @@ generate_log_file() {
 }
 
 models=(
+    "meta-llama/Llama-3.2-1B"
+    "meta-llama/Llama-3.2-3B"
     "models/meta-llama/Llama-2-7b-hf"
     "models/meta-llama/Llama-3.1-8B"
-    "models/Qwen/Qwen1.5-7B"
-    "models/Qwen/Qwen2.5-7B"
     "models/google/gemma-7b"
     "models/mistralai/Ministral-8B-Instruct-2410"
 )
@@ -27,5 +26,5 @@ models=(
 for model_path in "${models[@]}"; do
   log_file=$(generate_log_file "$model_path")
   echo "Fine-tuning $model_path, logs will be saved to $log_file"
-  python -u evaluation/evasion_finetune.py "$model_path" -t $num1m $num5m $num10m $num50m $num100m $num200m > "$log_file"
+  python -u evaluation/evasion_finetune.py "$model_path" -t $num1m $num5m $num10m $num50m $num100m > "$log_file"
 done
